80905dbd964011af884c4d5073ea95fb052418fe
speckoh <speckohart@gmail.com>
2023-01-19 15:20:00 -0500

    update

--- routes/hero-routes.js
+++ routes/hero-routes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const { handle404 } = require('../lib/custom-errors')
+
 const Hero = require('../models/hero')
 const router = express.Router()
 
@@ -16,6 +18,7 @@ router.get('/heroes', (req, res, next) => {
 
 router.get('/heroes/:id', (req, res, next) => {
     Hero.findById(req.params.id)
+    .then(handle404)
     .then(hero => {
         res.status(200).json({ hero: hero })
     })
@@ -25,9 +28,19 @@ router.get('/heroes/:id', (req, res, next) => {
 //request //response //next is middleware
 router.post('/heroes', (req, res, next) =>{
     Hero.create(req.body.hero)
+    .then(hero => {
+        res.status(201).json({ hero: hero })
+    })
+    .catch(next)
+})
+
+router.delete('/heroes/:id', (req, res, next) => {
+    Hero.findById(req.params.id)
+        .then(handle404)
         .then(hero => {
-            res.status(201).json({ hero: hero })
+            return hero.deleteOne()
         })
+        .then(() => res.sendStatus(204))
         .catch(next)
 })
 
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 //command center
 const express = require('express')
 const mongoose = require('mongoose')
-
 const cors = require('cors')
-const db = require('./config/db')
-const heroRoutes = require('./routes/hero-routes')
 
+const db = require('./config/db')
 const PORT = 8002
+
+const heroRoutes = require('./routes/hero-routes')
+const requestLogger = require('./lib/request-logger')
+const heroSeed = require('./lib/hero-seed')
 
 //deprecation warning
 mongoose.set('strictQuery', true)
@@ -19,8 +21,11 @@ mongoose.connect(db, {
 const app = express()
 
 app.use(cors({origin: `http://127.0.0.1:5500`}))
+app.use(requestLogger)
 app.use(express.json())
 app.use(heroRoutes)
+
+app.use('/seed', heroSeed)
 
 app.listen(PORT, () => {
     console.log('listening on ' + PORT)
